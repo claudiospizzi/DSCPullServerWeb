1c5559a9322640d97ce9eca51d051a80cb5154cc
Claudio Spizzi <claudio.spizzi@live.com>
2016-11-21 15:18:05 +0100

    Fix stale module import reference

--- Scripts/debug.default.ps1
+++ Scripts/debug.default.ps1
@@ -1,21 +1,21 @@
 
 # Get and dot source all helper functions (private)
 Split-Path -Path $PSScriptRoot |
-    Join-Path -ChildPath 'Modules\SecurityFever\Helpers' |
+    Join-Path -ChildPath 'Modules\DSCPullServerWeb\Helpers' |
         Get-ChildItem -Include '*.ps1' -Exclude '*.Tests.*' -Recurse |
             ForEach-Object { . $_.FullName }
 
 # Get and dot source all external functions (public)
 Split-Path -Path $PSScriptRoot |
-    Join-Path -ChildPath 'Modules\SecurityFever\Functions' |
+    Join-Path -ChildPath 'Modules\DSCPullServerWeb\Functions' |
         Get-ChildItem -Include '*.ps1' -Exclude '*.Tests.*' -Recurse |
             ForEach-Object { . $_.FullName }
 
 # Update format data
-Update-FormatData "$PSScriptRoot\..\Modules\SecurityFever\Resources\SecurityFever.Formats.ps1xml"
+Update-FormatData "$PSScriptRoot\..\Modules\DSCPullServerWeb\Resources\DSCPullServerWeb.Formats.ps1xml"
 
 # Update type data
-Update-TypeData "$PSScriptRoot\..\Modules\SecurityFever\Resources\SecurityFever.Types.ps1xml"
+Update-TypeData "$PSScriptRoot\..\Modules\DSCPullServerWeb\Resources\DSCPullServerWeb.Types.ps1xml"
 
 # Execute deubg
 # ToDo...
