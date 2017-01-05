432863c96fea9f2f5bf167824f32f675e6926f25
Claudio Spizzi <claudio.spizzi@live.com>
2017-01-05 11:20:09 +0100

    Update module, load helper functions

--- Modules/DSCPullServerWeb/DSCPullServerWeb.psm1
+++ Modules/DSCPullServerWeb/DSCPullServerWeb.psm1
@@ -1,9 +1,9 @@
 
 # Get and dot source all helper functions (private)
-# Split-Path -Path $PSCommandPath |
-#     Join-Path -ChildPath 'Helpers' |
-#         Get-ChildItem -Include '*.ps1' -Exclude '*.Tests.*' -Recurse |
-#             ForEach-Object { . $_.FullName }
+Split-Path -Path $PSCommandPath |
+    Join-Path -ChildPath 'Helpers' |
+        Get-ChildItem -Include '*.ps1' -Exclude '*.Tests.*' -Recurse |
+            ForEach-Object { . $_.FullName }
 
 # Get and dot source all external functions (public)
 Split-Path -Path $PSCommandPath |
